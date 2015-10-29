c1b0ebc8a3a08a22e9ddcf3f6e7967f3af6a164f
Kevin Wang <wangkevin448@gmail.com>
2015-10-28 18:20:33 -0700

    binary operation cannot be applied

--- src/network/matrix.rs
+++ src/network/matrix.rs
@@ -1,13 +1,43 @@
-struct M<T> {
+use std::ops;
+
+
+struct M<T: Clone + ops::Add + ops::Mul> {
     dim: (usize, usize),
-    data: [[T]],
+    data: Vec<Vec<T>>,
 }
 
-impl <T> M<T>  {
-    fn new(x: usize, y: usize, z: T) -> M<T> {
+impl <T: Clone + ops::Add + ops::Mul> M<T>  {
+    fn new(x: usize, y: usize, z: T) -> Self {
         M {
             dim: (x, y),
-            data: [[z; y]; x]
+            data: vec![vec![z; y]; x],
         }
+    }
+}
+
+impl <T: Clone + ops::Add + ops::Mul> ops::Add for M<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+
+    }
+}
+
+impl <T: Clone + ops::Add + ops::Mul> ops::Mul for M<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+
+    }
+}
+
+
+struct V<T>(Vec<T>);
+
+impl<T: Clone + ops::Add + ops::Mul> ops::Mul for V<T> {
+    type Output = T;
+
+    fn mul(self, rhs: Self) -> T {
+        self.0.iter().zip(rhs.0.iter()).map(|(x, y)| x*y).sum();
     }
 }
